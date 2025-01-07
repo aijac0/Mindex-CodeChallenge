3920aa10cf3265109bef914d4df8e4cb69ac365c
aijac0 <aidancollinscs@gmail.com>
2025-01-06 19:53:18 -0600

    Builder for Compensation database

--- CodeChallenge/Config/WebApplicationBuilderExt.cs
+++ CodeChallenge/Config/WebApplicationBuilderExt.cs
@@ -8,12 +8,16 @@ namespace CodeChallenge.Config
 {
     public static class WebApplicationBuilderExt
     {
-        private static readonly string DB_NAME = "EmployeeDB";
         public static void UseEmployeeDB(this WebApplicationBuilder builder)
         {
-            builder.Services.AddDbContext<EmployeeContext>(options =>
+            builder.Services.AddDbContext<EmployeeContext>(options => { options.UseInMemoryDatabase("EmployeeDB"); });
+        }
+
+        public static void UseCompensationDB(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<CompensationContext>(options =>
             {
-                options.UseInMemoryDatabase(DB_NAME);
+                options.UseInMemoryDatabase("CompensationDB");
             });
         }
     }
